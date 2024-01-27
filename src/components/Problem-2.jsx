8449a9884d5b80701be3e7f053cda0b02d68efe5
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2024-01-28 00:55:41 +0600

    Add axios dependency and implement basic functionality in Problem-2 component

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
 const Problem2 = () => {
+  const [showModalA, setShowModalA] = useState(false);
+  const [showModalB, setShowModalB] = useState(false);
+  const [contacts, setContacts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [onlyEven, setOnlyEven] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
-    return (
+ 
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+        <div className="d-flex justify-content-center gap-3">
+          <button className="btn btn-lg btn-outline-primary" type="button">
+            All Contacts
+          </button>
+          <button className="btn btn-lg btn-outline-warning" type="button">
+            US Contacts
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Problem2;
