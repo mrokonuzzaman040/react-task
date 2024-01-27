48403c9e47479e2b4637177ea0cccee1cc2c07ff
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2024-01-28 00:43:54 +0600

    Fix typo in Problem-1.jsx

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -32,7 +32,7 @@ const Problem1 = () => {
       <div className="row justify-content-center mt-5">
         <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
 
-        {/* Form Section  */}
+        {/* From Section */}
         <div className="col-6 ">
           <form
             className="row gy-2 gx-3 align-items-center mb-4"
@@ -70,7 +70,7 @@ const Problem1 = () => {
           <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
             <li className="nav-item">
               <button
-                className={`nav-link ${show === "all" && "Active"}`}
+                className={`nav-link ${show === "all" && "active"}`}
                 type="button"
                 onClick={() => handleClick("all")}
               >
@@ -79,18 +79,18 @@ const Problem1 = () => {
             </li>
             <li className="nav-item">
               <button
-                className={`nav-link ${show === "Active" && "Active"}`}
+                className={`nav-link ${show === "active" && "active"}`}
                 type="button"
-                onClick={() => handleClick("Active")}
+                onClick={() => handleClick("active")}
               >
                 Active
               </button>
             </li>
             <li className="nav-item">
               <button
-                className={`nav-link ${show === "Completed" && "Active"}`}
+                className={`nav-link ${show === "completed" && "active"}`}
                 type="button"
-                onClick={() => handleClick("Completed")}
+                onClick={() => handleClick("completed")}
               >
                 Completed
               </button>
